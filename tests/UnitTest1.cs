7043d300aa55771520f53c9ae2099b20680c14e6
killdns <kirill@danshin.xyz>
2023-02-06 20:58:21 +0400

    add empty test project

---
+++ tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace KillDNS.CaptchaSolver.Core.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
